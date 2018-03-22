5f8a30ad8633ef010d8162b7e03ae709c616ad5e
Samuel James Thompson <noreply@gmail.com>
2018-03-22 15:18:56 +0200

    Define variables and restructure comments

--- designs.js
+++ designs.js
@@ -1,11 +1,9 @@
-//Define your variables by selecting the DOM elements that the user will interact with. This is where your DOM skills can come into play! For instance, the submit button, the table, and the color picker need to be accessed. The value of the color selected needs to be stored as well, since the clicked cell in the table needs to be set to the selected color.
-
-//Add event listeners to the relevant DOM elements, so that user input can be color values and table sizes can be dynamically set by the user.
-
-//Set the size of the cross stitch canvas as an N by M grid with the makeGrid() function. Use your knowledge of JavaScript loops to dynamically clear and create the table based on user input. Each cell should have an event listener that sets the background color of the cell to the selected color.
-
-// Select color input
-// Select size input
+//Define variables required
+const submit = document.querySelector('.submit-button');
+const height = document.querySelector('.inputHeight').value;
+const width = document.querySelector('.inputWidth').value;
+let color = document.querySelector('.colorPicker').value;
+const table = document.querySelector('.pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 
@@ -14,3 +12,9 @@ function makeGrid() {
 // Your code goes here!
 
 }
+
+//Event listener for submit - makeGrid()
+
+//Event listener for mousedown within table on cells (background color change)
+
+//Event listener for doubleclick to reset cell to white
